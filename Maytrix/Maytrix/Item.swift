05fd7654abc575304abce30ef822cd820915bbac
googlegeminilist-crypto <googlegemini.list@tj.mell.net>
2026-03-02 14:41:38 +0000

    cool

---
+++ Maytrix/Maytrix/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Maytrix
+//
+//  Created by Thomas Mellor on 02/03/2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ Maytrix/MaytrixTests/MaytrixTests.swift
@@ -0,0 +1,17 @@
+//
+//  MaytrixTests.swift
+//  MaytrixTests
+//
+//  Created by Thomas Mellor on 02/03/2026.
+//
+
+import Testing
+@testable import Maytrix
+
+struct MaytrixTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
